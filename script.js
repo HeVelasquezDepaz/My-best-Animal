c1f24ce477d4adc48ee01a1fbe9f8ddc20582965
HeVelasquezDepaz <60240645+HeVelasquezDepaz@users.noreply.github.com>
2020-01-23 13:58:18 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,12 @@
+$(".sentence").click(function() {
+    $(".sentence").slideUp(" My Lion!");
+});
+
+
+$("img").click(function() {
+    $("img").hide();
+});
+
+$("h1").click(function() {
+    $(".LionPicture")(" ");
+});
